30d9929c5da005567d3e6a9789b1754c5cbc172c
Sina Kuşoğlu <sina.kusoglu@avkaryazilim.com.tr>
2023-10-13 22:25:58 +0300

    Player zıplama mekanizması güncellendi.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,41 @@ public abstract class Player : MonoBehaviour
 {
     [SerializeField] public float movementSpeed = 10f;
     [SerializeField] public float jumpSpeed = 5f;
+    private Rigidbody rb;
+    private Vector3 jumpVector;
+    private bool isGrounded;
 
     protected abstract float GetMovementSpeed();
     protected abstract float GetJumpSpeed();
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jumpVector = new Vector3(0f, 1f);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontal * Time.deltaTime * GetMovementSpeed(), vertical * Time.deltaTime * GetJumpSpeed(), 0f);
+        Vector3 movement = new Vector3(horizontal * Time.deltaTime * GetMovementSpeed(), 0f, 0f);
         transform.position += movement;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jumpVector * GetJumpSpeed(), ForceMode.Impulse);
+        }
     }
+
+
 
 
 }
