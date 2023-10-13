7d1b6f70ab7da55480c5b77ca97c2caf32aead65
yigit.ture <yigit.ture@avkaryazilim.com.tr>
2023-10-13 21:09:32 +0300

    Kameranın Z eksenini de takip etmesi sağlandı. Player icin rigidbodyler en ust elemente tasindi

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraScript : MonoBehaviour
 {
     [SerializeField] public Player _player;
 
 
-    private void Update()
+    private void FixedUpdate()
     {
-        //ofset = transform.position - _player.transform.position;
-        //transform.position = _player.transform.position + ofset;
-        Vector3 moveCam = new Vector3(_player.transform.position.x, 0, 0);
-        transform.position = moveCam + new Vector3(0,0, -10);
+        transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y, -10);
     }
+
+
 }
